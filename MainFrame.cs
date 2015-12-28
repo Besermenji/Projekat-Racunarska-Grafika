9007c42633ec167e5e248f564c15e5dcdc7a550f
Besermenji <besermenji.vladimir@gmail.com>
2015-12-28 21:39:29 +0100

    dodato to da se ne vidi donja strana piste i da se ogranici koliko se moze ubliziti/odaljiti od centra

--- MainFrame.cs
+++ MainFrame.cs
@@ -19,17 +19,19 @@ namespace Projekat
         {
             InitializeComponent();
             canvas.InitializeContexts();
-            try {
-                world = new World(@"C:\Users\Vladimir\Desktop\Racunarska grafika\Projekat\Projekat\Projekat\star wars x-wing", "star wars x-wing.3ds", canvas.Width,canvas.Height);
+            try
+            {
+                world = new World(@"C:\Users\Vladimir\Desktop\Racunarska grafika\Projekat\Projekat\Projekat\star wars x-wing", "star wars x-wing.3ds", canvas.Width, canvas.Height);
             }
-            catch {
+            catch
+            {
                 MessageBox.Show("Neuspesno kreirana instanca OpenGL sveta", "GRESKA", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 this.Close();
             }
         }
 
 
-        
+
         private void canvas_Paint(object sender, PaintEventArgs e)
         {
             world.Draw();
@@ -43,57 +45,135 @@ namespace Projekat
             world.Resize();
         }
 
-       
+
 
         private void MainFrame_Load(object sender, EventArgs e)
         {
 
         }
 
+
+
+
         private void canvas_KeyDown(object sender, KeyEventArgs e)
         {
 
-            int maxDistance = 21;
+
             switch (e.KeyCode)
             {
                 case Keys.F2: this.Close(); break;
-                case Keys.W: world.RotationX -= 4.0f; break;
-                case Keys.S: world.RotationX += 4.0f; break;
+                case Keys.W:
+                    {
+
+
+                        if (world.RotationX > 0)
+                        {
+                            if (world.RotationX <= 19)
+                            {
+                                world.RotationX -= 4.0f;
+                            }
+                        }
+                        else
+                        {
+
+                            if (world.RotationX > -157)
+                            {
+                                world.RotationX -= 4;
+                            }
+                        }
+
+
+                        //world.RotationX -= 4.0f;
+
+
+
+                        break;
+                    }
+                case Keys.S:
+                    {
+
+
+                        if (world.RotationX > 0)
+                        {
+                            if (world.RotationX < 19)
+                            {
+                                world.RotationX += 4.0f;
+                            }
+                        }
+                        else
+                        {
+
+                            if (world.RotationX >= -157)
+                            {
+                                world.RotationX += 4;
+                            }
+                        }
+
+
+                        //world.RotationX -= 4.0f;
+
+
+
+                        break;
+                    }
                 case Keys.A: world.RotationY -= 4.0f; break;
                 case Keys.D: world.RotationY += 4.0f; break;
-                case Keys.Subtract: {
-                      
+                case Keys.Subtract:
+                    {
+                        if (Math.Floor(Math.Abs(world.cameraControl)) == 10)
+                        {
+                            //world.cameraControl = 10;
+                        }
+                        else
+                        {
                             world.cameraControl -= .3f;
-                       
-
-                        break; }
+                        }
+                        break;
+                    }
                 case Keys.OemMinus:
                     {
 
-                        world.cameraControl -= .3f;
-
-
+                        if (Math.Floor(Math.Abs(world.cameraControl)) == 10)
+                        {
+                            //world.cameraControl = 10;
+                        }
+                        else
+                        {
+                            world.cameraControl -= .3f;
+                        }
                         break;
                     }
                 case Keys.Oemplus:
                     {
 
-                        world.cameraControl += .3f;
-
-
+                        if (Math.Floor(Math.Abs(world.cameraControl)) == 0)
+                        {
+                            //world.cameraControl = 0;
+                        }
+                        else
+                        {
+                            world.cameraControl += .3f;
+                        }
                         break;
                     }
                 case Keys.Add:
                     {
-                        
-                        world.cameraControl += .3f;
-
-
+                        if (Math.Floor(Math.Abs(world.cameraControl)) == 0)
+                        {
+                            //world.cameraControl = 0;
+                        }
+                        else
+                        {
+                            world.cameraControl += .3f;
+                        }
                         break;
                     }
 
 
             }
+
+
+
 
             this.Refresh();
         }
@@ -101,7 +181,7 @@ namespace Projekat
         private void colorButton_Click(object sender, EventArgs e)
         {
             ColorDialog cd = new ColorDialog();
-            
+
             if (cd.ShowDialog() == DialogResult.OK)
             {
                 world.light_red = cd.Color.R;
