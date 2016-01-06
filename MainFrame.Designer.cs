491b56b9ef2603a3f7435435a82fdbcd855193b4
Besermenji <besermenji.vladimir@gmail.com>
2016-01-06 14:38:18 +0100

    potrebno refraktorisati kod

---
+++ MainFrame.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Projekat
+{
+    partial class MainFrame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.canvas = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.lightGroupBox = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lightsInterval = new System.Windows.Forms.NumericUpDown();
+            this.lightTimer = new System.Windows.Forms.Timer(this.components);
+            this.airplaneTimer = new System.Windows.Forms.Timer(this.components);
+            this.lightGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightsInterval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.AccumBits = ((byte)(0));
+            this.canvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.canvas.AutoCheckErrors = false;
+            this.canvas.AutoFinish = false;
+            this.canvas.AutoMakeCurrent = true;
+            this.canvas.AutoSwapBuffers = true;
+            this.canvas.BackColor = System.Drawing.SystemColors.WindowText;
+            this.canvas.ColorBits = ((byte)(32));
+            this.canvas.DepthBits = ((byte)(16));
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(366, 364);
+            this.canvas.StencilBits = ((byte)(0));
+            this.canvas.TabIndex = 0;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.KeyDown += new System.Windows.Forms.KeyEventHandler(this.canvas_KeyDown);
+            this.canvas.Resize += new System.EventHandler(this.canvas_Resize);
+            // 
+            // colorButton
+            // 
+            this.colorButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.colorButton.Location = new System.Drawing.Point(110, 182);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(75, 23);
+            this.colorButton.TabIndex = 1;
+            this.colorButton.Text = "Boja sijalica";
+            this.colorButton.UseVisualStyleBackColor = true;
+            this.colorButton.Click += new System.EventHandler(this.colorButton_Click);
+            // 
+            // lightGroupBox
+            // 
+            this.lightGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lightGroupBox.Controls.Add(this.numericUpDown1);
+            this.lightGroupBox.Controls.Add(this.label2);
+            this.lightGroupBox.Controls.Add(this.label1);
+            this.lightGroupBox.Controls.Add(this.lightsInterval);
+            this.lightGroupBox.Controls.Add(this.colorButton);
+            this.lightGroupBox.Location = new System.Drawing.Point(391, 138);
+            this.lightGroupBox.Name = "lightGroupBox";
+            this.lightGroupBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lightGroupBox.Size = new System.Drawing.Size(200, 211);
+            this.lightGroupBox.TabIndex = 2;
+            this.lightGroupBox.TabStop = false;
+            this.lightGroupBox.Text = "Rad sa sijalicama";
+            this.lightGroupBox.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            131072});
+            this.numericUpDown1.Location = new System.Drawing.Point(9, 101);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Veličina sijalica";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Interval paljenja/gašenja sijalica (n/sec)";
+            // 
+            // lightsInterval
+            // 
+            this.lightsInterval.Location = new System.Drawing.Point(6, 50);
+            this.lightsInterval.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.lightsInterval.Name = "lightsInterval";
+            this.lightsInterval.ReadOnly = true;
+            this.lightsInterval.Size = new System.Drawing.Size(120, 20);
+            this.lightsInterval.TabIndex = 2;
+            this.lightsInterval.ValueChanged += new System.EventHandler(this.lightsInterval_ValueChanged);
+            // 
+            // lightTimer
+            // 
+            this.lightTimer.Tick += new System.EventHandler(this.lightTimer_Tick);
+            // 
+            // airplaneTimer
+            // 
+            this.airplaneTimer.Tick += new System.EventHandler(this.airplaneTimer_Tick);
+            // 
+            // MainFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 361);
+            this.Controls.Add(this.lightGroupBox);
+            this.Controls.Add(this.canvas);
+            this.Name = "MainFrame";
+            this.Text = "Projekat";
+            this.Load += new System.EventHandler(this.MainFrame_Load);
+            this.lightGroupBox.ResumeLayout(false);
+            this.lightGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightsInterval)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl canvas;
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.GroupBox lightGroupBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown lightsInterval;
+        private System.Windows.Forms.Timer lightTimer;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer airplaneTimer;
+    }
+}
+
---
+++ MainFrame.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekat
+{
+    public partial class MainFrame : Form
+    {
+        World world = null;
+        bool par = false;
+        long animation_duration = 50;
+        //promenjiva koju koristimo kada je animacija u toku
+        bool animation_in_progress = false;
+
+        public MainFrame()
+        {
+            InitializeComponent();
+            canvas.InitializeContexts();
+            try
+            {
+                world = new World(@"C:\Users\Vladimir\Desktop\Racunarska grafika\Projekat\Projekat\Projekat\star wars x-wing", "star wars x-wing.3ds", canvas.Width, canvas.Height);
+            }
+            catch
+            {
+                MessageBox.Show("Neuspesno kreirana instanca OpenGL sveta", "GRESKA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+        }
+
+
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            world.Draw();
+        }
+
+        private void canvas_Resize(object sender, EventArgs e)
+        {
+            base.OnResize(e);
+            world.Height = canvas.Height;
+            world.Width = canvas.Width;
+            world.Resize();
+        }
+
+
+
+        private void MainFrame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (!animation_in_progress)
+            {
+                switch (e.KeyCode)
+                {
+
+                    case Keys.B:
+                        {
+                            airplaneTimer.Enabled = true;
+                            animation_duration = 60;
+                            break;
+                        }
+                    case Keys.F2: this.Close(); break;
+                    case Keys.W:
+                        {
+
+
+                            if (world.RotationX > 0)
+                            {
+                                if (world.RotationX <= 19)
+                                {
+                                    world.RotationX -= 4.0f;
+                                }
+                            }
+                            else
+                            {
+
+                                if (world.RotationX > -157)
+                                {
+                                    world.RotationX -= 4;
+                                }
+                            }
+
+
+                            //world.RotationX -= 4.0f;
+
+
+
+                            break;
+                        }
+                    case Keys.S:
+                        {
+
+
+                            if (world.RotationX > 0)
+                            {
+                                if (world.RotationX < 19)
+                                {
+                                    world.RotationX += 4.0f;
+                                }
+                            }
+                            else
+                            {
+
+                                if (world.RotationX >= -157)
+                                {
+                                    world.RotationX += 4;
+                                }
+                            }
+
+
+                            //world.RotationX -= 4.0f;
+
+
+
+                            break;
+                        }
+                    case Keys.A: world.RotationY -= 4.0f; break;
+                    case Keys.D: world.RotationY += 4.0f; break;
+                    case Keys.Subtract:
+                        {
+                            if (Math.Floor(Math.Abs(world.cameraControl)) == 10)
+                            {
+                                //world.cameraControl = 10;
+                            }
+                            else
+                            {
+                                world.cameraControl -= .3f;
+                            }
+                            break;
+                        }
+                    case Keys.OemMinus:
+                        {
+
+                            if (Math.Floor(Math.Abs(world.cameraControl)) == 10)
+                            {
+                                //world.cameraControl = 10;
+                            }
+                            else
+                            {
+                                world.cameraControl -= .3f;
+                            }
+                            break;
+                        }
+                    case Keys.Oemplus:
+                        {
+
+                            if (Math.Floor(Math.Abs(world.cameraControl)) == 0)
+                            {
+                                //world.cameraControl = 0;
+                            }
+                            else
+                            {
+                                world.cameraControl += .3f;
+                            }
+                            break;
+                        }
+                    case Keys.Add:
+                        {
+                            if (Math.Floor(Math.Abs(world.cameraControl)) == 0)
+                            {
+                                //world.cameraControl = 0;
+                            }
+                            else
+                            {
+                                world.cameraControl += .3f;
+                            }
+                            break;
+                        }
+
+
+                }
+
+
+
+
+                this.Refresh();
+            }
+        }
+
+        private void colorButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                world.light_red = cd.Color.R;
+                world.light_green = cd.Color.G;
+                world.light_blue = cd.Color.B;
+                this.Refresh();
+                //MessageBox.Show("RED: " + red);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lightsInterval_ValueChanged(object sender, EventArgs e)
+        {
+            if (lightsInterval.Value == 0)
+            {
+                lightTimer.Enabled = false;
+                world.lights_enabled = false;
+            }
+            else {
+                lightTimer.Enabled = true;
+                lightTimer.Interval = (int)Math.Round(500 / lightsInterval.Value);
+                world.lights_enabled = true;
+            }
+        }
+
+        private void lightTimer_Tick(object sender, EventArgs e)
+        {
+            ////test da li radi tajmer
+            if (par)
+            {
+                world.lights_enabled = true;
+                //par = false;
+            }
+            else {
+                //par = true;
+                world.lights_enabled = false;
+            }
+
+            par = !par;
+            //Random r = new Random();
+            //world.light_red = (byte)r.Next(0, 255);
+            //world.light_blue = (byte)r.Next(0, 255);
+            //world.light_green = (byte)r.Next(0, 255);
+            this.Refresh();
+
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            world.light_diameter = (float)numericUpDown1.Value;
+            this.Refresh();
+        }
+
+        private void airplaneTimer_Tick(object sender, EventArgs e)
+        {
+            if (animation_duration != 0)
+            {
+                world.airplane_pos[1] += 1f;
+                world.airplane_pos[2] -= 5f;
+                world.airplane_light_pos[1] += 1f;
+                world.airplane_light_pos[2] -= 5f;
+                //disejblovanje korisnicke interakcije
+                animation_in_progress = true;
+                lightGroupBox.Enabled = false;
+
+                animation_duration--;
+
+            }
+            else {
+                animation_in_progress = false;
+                lightGroupBox.Enabled = true;
+                airplaneTimer.Enabled = false;
+                world.airplane_pos[1] = -8.5f;
+                world.airplane_pos[2] = -10f;
+                world.airplane_light_pos[1] = 0f;
+                world.airplane_light_pos[2] = -10f;
+            }
+            this.Refresh();
+            
+            
+        }
+    }
+}
